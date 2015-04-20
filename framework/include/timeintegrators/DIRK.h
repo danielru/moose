919659e63324ae74a9e4dca3882762296055a52e
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-04-20 15:01:02 +0200

    created two new files DIRK.C and DIRK.h ; at the moment they are identical to the ImplicitEuler files

---
+++ framework/include/timeintegrators/DIRK.h
@@ -0,0 +1,43 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef DIRK_H
+#define DIRK_H
+
+#include "TimeIntegrator.h"
+
+class DIRK;
+
+template<>
+InputParameters validParams<DIRK>();
+
+/**
+ * Implicit Euler's method
+ */
+class DIRK : public TimeIntegrator
+{
+public:
+  DIRK(const std::string & name, InputParameters parameters);
+  virtual ~DIRK();
+
+  virtual int order() { return 1; }
+  virtual void computeTimeDerivatives();
+  virtual void postStep(NumericVector<Number> & residual);
+
+protected:
+
+};
+
+
+#endif /* DIRK_H */
